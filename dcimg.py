eda7596f4c80a323fb40387bac2e7d7fed7b6174
Giacomo Mazzamuto <mazzamuto@lens.unifi.it>
2016-12-22 10:41:16 +0100

    add DcimgFile class, parse header

---
+++ dcimg.py
@@ -0,0 +1,83 @@
+import mmap
+
+
+class DcimgFile(object):
+    """A DCIMG file (Hamamatsu format), memory-mapped.
+
+    After use, call the close() method to release resources properly.
+    """
+
+    def __init__(self, file_name=None):
+        self.file_name = file_name
+        self.mm = None  #: memory-mapped array
+        self.nfrms = None  #: number of frames
+        self.file_size = None
+        self.footer_loc = None
+        self.byte_depth = None  #: number of bytes per pixel
+        self.x_size = None
+        self.y_size = None
+        self.bytes_per_row = None
+        self.bytes_per_img = None
+
+    def open(self, file_name=None):
+        if file_name is None:
+            file_name = self.file_name
+
+        with open(file_name, 'r') as f:
+            mm = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
+            if mm[:5] != b"DCIMG":
+                raise RuntimeError("Invalid DCIMG file")
+
+        self.mm = mm
+        self._parse_header()
+
+    def close(self):
+        if self.mm is not None:
+            self.mm.close()
+        self.mm = None
+
+    def _parse_header(self):
+        bytes_to_skip = 4 * int.from_bytes(self.mm[8:12], byteorder="little")
+
+        index = 8 + bytes_to_skip
+        self.nfrms = int.from_bytes(self.mm[index:index + 4],
+                                    byteorder="little")
+
+        index = 48
+        self.file_size = int.from_bytes(self.mm[index:index + 4],
+                                        byteorder="little")
+
+        index = 120
+        self.footer_loc = int.from_bytes(self.mm[index:index + 4],
+                                         byteorder="little")
+
+        index = 156
+        self.byte_depth = int.from_bytes(self.mm[index:index + 4],
+                                         byteorder="little")
+
+        index = 164
+        self.x_size = int.from_bytes(self.mm[index:index + 4],
+                                     byteorder="little")
+
+        index = 168
+        self.bytes_per_row = int.from_bytes(self.mm[index:index + 4],
+                                            byteorder="little")
+
+        index = 172
+        self.y_size = int.from_bytes(self.mm[index:index + 4],
+                                     byteorder="little")
+
+        index = 176
+        self.bytes_per_img = int.from_bytes(self.mm[index:index + 4],
+                                            byteorder="little")
+
+        if self.bytes_per_row != self.byte_depth * self.y_size:
+            e_str = "bytes_per_row ({bytes_per_row}) " \
+                    "!= byte_depth ({byte_depth}) * nrows ({y_size})" \
+                .format(**vars(self))
+            raise RuntimeError(e_str)
+
+        if self.bytes_per_img != self.bytes_per_row * self.y_size:
+            e_str = "bytes per img ({bytes_per_img}) != nrows ({y_size}) * " \
+                    "bytes_per_row ({bytes_per_row})".format(**vars(self))
+            raise RuntimeError(e_str)
