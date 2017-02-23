e6b81fb6a81b1ce45fa723b06a294785e5c0cbb7
Giacomo Mazzamuto <mazzamuto@lens.unifi.it>
2017-02-23 10:49:16 +0100

    version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dcimg',
-    version='0.1.0',
+    version='0.1.1',
     description='Python module to read Hamamatsu DCIMG files',
     long_description='Python module to read Hamamatsu DCIMG files',
     url='',
